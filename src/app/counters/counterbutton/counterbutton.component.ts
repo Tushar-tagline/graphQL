0ef635dcd55fdb8f9b0cbf30e285fc9bfc1d4035
tushar-tagline <tushar.tagline@gmail.com>
2022-04-06 14:47:59 +0530

    create counterbutton component and dispatch the data

---
+++ src/app/counters/counterbutton/counterbutton.component.ts
@@ -0,0 +1,32 @@
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from 'src/app/state/couter.action';
+
+@Component({
+  selector: 'app-counterbutton',
+  templateUrl: './counterbutton.component.html',
+  styleUrls: ['./counterbutton.component.scss']
+})
+export class CounterbuttonComponent implements OnInit {
+  //@Output() increment =new EventEmitter<void>();
+  //@Output() decrement =new EventEmitter<void>();
+  //@Output() reset =new EventEmitter<void>();
+  constructor(private store: Store<{ counter: { counter: number } }>) { }
+
+  ngOnInit(): void {
+  }
+  public onPlus(): void {
+    // this.increment.emit()
+    this.store.dispatch(increment())
+  }
+  public onMin(): void {
+    //this.decrement.emit()`
+    this.store.dispatch(decrement())
+  }
+
+  public onReset(): void {
+    //this.reset.emit()
+    this.store.dispatch(reset())
+  }
+}
