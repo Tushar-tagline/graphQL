18e7b35f43962988ce34cd0fd75bf30049de6fe2
tushar-tagline <tushar.tagline@gmail.com>
2022-04-06 14:53:42 +0530

    create counter reducer file

---
+++ src/app/state/couter.reducer.ts
@@ -0,0 +1,26 @@
+import { state } from "@angular/animations";
+import { createReducer, on } from "@ngrx/store";
+
+import { decrement, increment, reset } from "./couter.action";
+import { initialstate } from "./couter.state";
+
+const _counterReducer = createReducer(initialstate, on(increment, (state) => {
+    return {
+        ...state,
+        counter: state.counter + 1
+    }
+}), on(decrement, (state) => {
+    return {
+        ...state,   
+        counter: state.counter - 1
+    }
+}), on(reset, (state) => {
+    return {
+        ...state,
+        counter: 0
+    }
+})
+)
+export function counterReducer(state: any, action: any) {
+    return _counterReducer(state, action)
+}
