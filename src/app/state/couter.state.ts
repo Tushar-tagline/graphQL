afafaaa1713d1b6c753e70a5858010faf3d7c872
tushar-tagline <tushar.tagline@gmail.com>
2022-04-07 14:40:04 +0530

    create changename state in counterstate

--- src/app/state/couter.state.ts
+++ src/app/state/couter.state.ts
@@ -1,8 +1,10 @@
  export interface counterstate{
      counter:number
+     changname:any
  }
  
  
  export const initialstate:counterstate = {
-    counter:0
-}
+    counter:0,
+    changname:'who are you'
+}       
