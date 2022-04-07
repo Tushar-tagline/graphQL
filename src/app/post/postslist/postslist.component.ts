e1c9f150c0b7b43837b7c891a84065e6e48f6a42
tushar-tagline <tushar.tagline@gmail.com>
2022-04-07 14:38:07 +0530

    create postlist component

---
+++ src/app/post/postslist/postslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './postslist.component.html',
+  styleUrls: ['./postslist.component.scss']
+})
+export class PostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
