e0993cfbb13c24d1c0d55585a94acdf7211d705d
tushar-tagline <tushar.tagline@gmail.com>
2022-04-07 14:33:43 +0530

    create routing for all component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './counters/counter/counter.component';
+import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { PostsComponent } from './post/postslist/postslist.component';
 
 const routes: Routes = [
   {
     path:'',
+    component:HomeComponent
+    
+  },
+  {
+    path:'counter',
+    component:CounterComponent
+  },
+  {
+    path:'listdata',
     component:ListComponent
+  },
+  {
+    path:'posts',
+    component:PostsComponent
+  },
+  {
+    path:'**',
+    redirectTo:''
   }
 ];
 
